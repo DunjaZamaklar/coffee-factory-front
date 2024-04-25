d90fe7d8583fff902f2afe1029b6b0720af89517
DunjaZamaklar <56863991+DunjaZamaklar@users.noreply.github.com>
2024-04-25 17:46:37 +0200

    setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', loadComponent: () => import('./pages/login/login.component').then(component => component.LoginComponent)
+    },
+    {
+        path: 'pages',
+        loadComponent: () => import('./layout/main-layout/main-layout.component').then(component => component.MainLayoutComponent),
+        children: [
+            {
+                path: 'home',
+                loadComponent: () => import('./pages/home/home.component').then(component => component.HomeComponent)
+            }
+        ]
+    }
+];
