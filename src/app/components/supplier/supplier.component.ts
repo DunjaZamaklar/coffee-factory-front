c90997484f21a831748b973e443ce06c6c921e6f
subotinlenka <subotinlenka@gmail.com>
2024-07-31 00:00:32 +0200

    Home page change, Supplier component changes

--- src/app/components/supplier/supplier.component.ts
+++ src/app/components/supplier/supplier.component.ts
@@ -1,13 +1,44 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Supplier } from '../../../models/Supplier';
+import { Subscription } from 'rxjs';
+import { SupplierService } from '../../../services/Supplier.service';
 
 @Component({
   selector: 'app-component-supplier',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatToolbarModule],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.css'
 })
-export class SupplierComponent {
+export class SupplierComponent implements OnInit, OnDestroy {
 
+  displayedColumns = ['Id', 'Name', 'Address', 'City', 'Country', 'PhoneNumber' , 'Email'];
+
+  public subscription : Subscription = new Subscription();
+  public dataSource: MatTableDataSource<Supplier> = new MatTableDataSource<Supplier>();
+
+  constructor(private supplierService: SupplierService) { }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  public loadData() {
+    this.subscription = this.supplierService.getAllSuppliers().subscribe(
+      data => {
+          this.dataSource = new MatTableDataSource(data);
+      }
+    ),
+    (error: Error) => {
+      console.log(error.name + ' ' + error.message);
+    }
+  }
 
 }
